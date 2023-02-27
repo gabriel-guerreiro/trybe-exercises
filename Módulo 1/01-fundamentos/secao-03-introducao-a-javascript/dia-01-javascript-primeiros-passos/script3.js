55e6984e921df56bb44ccdcd4d25b2b2e0b25bdc
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-27 16:51:31 -0300

    exercícios do dia

---
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script3.js
@@ -0,0 +1,7 @@
+console.log(!(2 + 2) === 4);
+
+let squirtle = "melhor pokemon inicial";
+
+console.log(squirtle); // false
+
+console.log(!null);
---
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script4.js
@@ -0,0 +1,16 @@
+let status = 'lista';
+switch (status){
+    case 'aprovada':
+        console.log("parabens");
+        break;
+    case 'lista':
+        console.log("lista de espera");
+        break;
+    case 'reprovada':
+        console.log("reprovada, infelizmente");
+        break;
+
+    default:
+        console.log("informação incorreta");
+}
+
