bf15c4bf6e739edf5a070bae651ef894607ff9f3
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-28 01:19:00 -0300

    faltam aprimoramentos

--- Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/inss.js
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/inss.js
@@ -1,4 +1,4 @@
-let salariobruto = 3300.00;
+let salariobruto = 4000.00;
 let aliquota;
 let aliquotaIR;
 let dedutível;
@@ -15,27 +15,34 @@ if(salariobruto <= 1556.64){
         salarioliquido = salariobase - (salariobase * aliquotaIR - dedutível);
         console.log('Seu salário é isento de IR: R$' + salarioliquido + " e sua alíquota do INSS é de " + "8%");
     }
-} else if(salariobruto > 1556.64 && salariobruto <= 2594.92){
+} else if(salariobruto > 1556.64){
     aliquota = 0.09;
     salariobase = salariobruto - salariobruto * aliquota;
     if(salariobase > 1903.98 && salariobase <= 2826.65){
         aliquotaIR = 0.075;
         dedutível = 142.80;
         salarioliquido = salariobase - (salariobase * aliquotaIR - dedutível);
-        console.log('Seu salário tem alíquota de IR de 7,5% com dedutível de R$142,80, e INSS de 9% e seu valor é: '+ salarioliquido);
+        console.log('seu salário é: '+ salarioliquido);
     }
-} else if(salariobruto > 2594.92 && salariobruto <= 5189.82){
+} else if(salariobruto >= 2594.92){
     aliquota = 0.11;
     salariobase = salariobruto - salariobruto * aliquota;
     if(salariobase > 2826.66 && salariobase <= 3751.05){
         aliquotaIR = 0.15;
         dedutível = 353.80;
         salarioliquido = salariobase - (salariobase * aliquotaIR - dedutível);
-        console.log('Seu salário tem alíquota de IR de 15% com dedutível de R$353,80, e INSS de 11% e seu valor é: '+ salarioliquido);
+        console.log('seu salário é '+ salarioliquido);
 
     }
-} else if(salariobruto > 5189.82){
+} else if(salariobruto > 2594.92){
     aliquota = 0.15; //corrigir aliquota maxima 570 reais;
     salariobase = salariobruto - salariobruto * aliquota;
+    if(salariobase > 3751.05 && salariobase <= 4664.68){
+        aliquotaIR = 0.225;
+        dedutível = 636.13;
+        salarioliquido = salariobase - (salariobase * aliquotaIR - dedutível);
+        console.log("seu salário é " + salarioliquido);
+
+    }
 
 }
