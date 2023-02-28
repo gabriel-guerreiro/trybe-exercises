30485d6173d42aef271ad77ca221bfaa35aa8f5c
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-27 23:44:13 -0300

    corrige primeiro if

--- Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/inss.js
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/inss.js
@@ -1,12 +1,12 @@
 let salariobruto = 1300.00;
 let aliquota;
-let salariobase = salariobruto - salariobruto * aliquota;
+let salariobase;
 
 if(salariobruto <= 1556.64){
     //roda cálculo inss e retorna pra função abaixo
+    aliquota = 0.08;
+    salariobase = salariobruto - salariobruto * aliquota;
     if(salariobase <= 1903.98){
-        aliquota = 0.08;
-        salariobase = salariobruto - salariobruto * aliquota;
         console.log('Seu salário é isento de IR: R$' + salariobase + " e sua alíquota do INSS é de " + aliquota);
     }
-};
+}
