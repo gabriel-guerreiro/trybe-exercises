bf8035e1391f60c5e49b0126992c5b4cf9617957
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-28 19:35:51 -0300

    exercício numbers array

---
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/testes.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    //console.log(numbers[index]);
+    sum += numbers[index];
+    average = sum / (numbers.length + 1)
+}
+if (average > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
+
+
+console.log(sum);
+console.log(average);
