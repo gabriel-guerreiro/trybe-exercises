ca12ba27f00a8ad2cac1dd024297b48605327bd7
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-28 20:24:54 -0300

    funcional, adiconar maior valor

--- Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/numbersarray.js
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/numbersarray.js
@@ -1,12 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let average;
+let higher;
 
 for (let index = 0; index < numbers.length; index += 1) {
     //console.log(numbers[index]);
     sum += numbers[index];
     average = sum / (numbers.length + 1)
 }
+
 if (average > 20) {
     console.log('Valor maior que 20');
 } else {
@@ -16,3 +18,4 @@ if (average > 20) {
 
 console.log(sum);
 console.log(average);
+console.log(higher)
