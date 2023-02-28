f83695badfa95cc95e5a52d04c24375ab022411e
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-02-28 14:45:27 -0300

    arquivo novo

--- Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/impostoinss.js
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercícioimposto/impostoinss.js
@@ -1,4 +1,4 @@
-let salariobruto = 2230.00;
+let salariobruto = 3000.00;
 let aliquota;
 let aliquotaIR;
 let dedutível;
@@ -21,6 +21,18 @@ if (salariobruto <= 1556.64) {
 } else if (salariobruto > 5189.82) {
     aliquota = 570.88; 
     salariobase = salariobruto - salariobruto * aliquota;
+}
+
+console.log(salariobase);
+
+if (salariobase <= 1903.98){
+    aliquotaIR = 0;
+    dedutível = 0;
+    salariobase = salariobruto -(salariobruto * aliquotaIR - dedutível);
+} else if (salariobase > 1903.98 && salariobase <= 2826.65) {
+    aliquotaIR = 0.075;
+    dedutível = 142.80;
+    salariobase = salariobruto -(salariobruto * aliquotaIR - dedutível);
 }
 
 console.log(salariobase);
