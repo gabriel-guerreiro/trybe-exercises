a9ea3262ab3936f79da02fc93a619b37ee6f7e08
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-03-01 01:48:44 -0300

    adiciona array1a25

---
+++ Módulo 1/01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/array1a25.js
@@ -0,0 +1,12 @@
+let array = [];
+let result;
+
+for (let index = 1; index <= 25; index += 1){
+    array.push(index);
+    result = index / 2;
+    console.log(result);
+
+
+
+}
+console.log(array);
