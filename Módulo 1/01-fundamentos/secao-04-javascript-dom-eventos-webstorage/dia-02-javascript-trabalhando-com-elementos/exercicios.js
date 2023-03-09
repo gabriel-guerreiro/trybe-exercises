cd1acb6d5274713fabfdf267f830a76244bf0909
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-03-09 16:33:48 -0300

    fim

--- Módulo 1/01-fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-02-javascript-trabalhando-com-elementos/exercicios.js
+++ Módulo 1/01-fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-02-javascript-trabalhando-com-elementos/exercicios.js
@@ -12,4 +12,20 @@ console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
 
 console.log(document.getElementById("pai").childNodes[5]);
 
+let pai = document.getElementById('pai');
+let irmaoElementoOndeVoceEsta = document.createElement('section');
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let filhoelementoondevoceesta = document.createElement('section');
+filhoelementoondevoceesta.id = 'filhoelementoondevoceesta';
+elementoOndeVoceEsta.appendChild(filhoelementoondevoceesta);
+
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const primeiroFilhoDoFilhoDoFilho = document.createElement('section');
+primeiroFilhoDoFilhoDoFilho.id = 'primeiroFilhoDoFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(primeiroFilhoDoFilhoDoFilho);
+
+console.log(document.getElementById('primeiroFilhoDoFilhoDoFilho').parentNode.parentNode.nextSibling.nextSibling);
 
