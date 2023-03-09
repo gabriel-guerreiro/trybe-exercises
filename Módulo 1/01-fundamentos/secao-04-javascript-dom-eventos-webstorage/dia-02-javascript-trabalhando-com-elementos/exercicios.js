f8a6c47ddf2017ec5d3ab880cb966166a6eb028a
Gabriel Guerreiro <guerreirogabriel010203@gmail.com>
2023-03-09 15:47:28 -0300

    commit exercicios

---
+++ Módulo 1/01-fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-02-javascript-trabalhando-com-elementos/exercicios.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("elementoOndeVoceEsta").parentElement.style.color = "red";
+
+document.getElementById("primeiroFilhoDoFilho").innerText = "Texto aleatório";
+
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
+
+console.log(document.getElementById("pai").childNodes[5]);
+
+
